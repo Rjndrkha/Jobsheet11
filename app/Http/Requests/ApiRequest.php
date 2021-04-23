77bfdc5b4f8efaebdb61056980f61696b95c291e
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2021-04-23 22:00:36 +0700

    first commit

---
+++ app/Http/Requests/ApiRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class ApiRequest extends FormRequest
+{
+    use ApiResponse;
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    abstract public function rules();
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HtmlResponseException($this->apiError(
+            $validator->errors(),
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+        ));
+    }
+
+
+    protected function failedAuthorization()
+    {
+        throw new HtmlResponseException($this->apiError(
+            null,
+            Response::HTTP_UNAUTHORIZED
+        ));
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
