f9119def1d17614d0618526fddde93b30afd215c
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2021-04-23 22:05:12 +0700

    ADD

--- app/Http/Requests/ApiRequest.php
+++ app/Http/Requests/ApiRequest.php
@@ -32,15 +32,5 @@ abstract class ApiRequest extends FormRequest
         ));
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            //
-        ];
-    }
+    
 }
